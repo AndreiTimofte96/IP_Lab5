00c8fcfc69505f817f066e6c978e4b24559824d0
MihaiStan72 <stan.mihai72@gmail.com>
2018-03-19 15:28:56 +0200

    implement cart

--- implementation/src/Cart.java
+++ implementation/src/Cart.java
@@ -4,22 +4,29 @@ public class Cart {
 
   public java.util.Vector products;
 
-    public Vector  myUser;
-
   public Integer getPrice() {
-  return null;
+    Integer sum = 0;
+    for (int i = 0; i < products.length; i++) {
+        sum += products[i].price;
+    }
+    return sum;
   }
 
   public void checkout() {
+      products = null;
+      //GGGGGGGGGGGGGGGGGGGGGGGGGGG
   }
 
   public void addProduct(Product product) {
+      products.add(product);
   }
 
   public void removeProduct(Product product) {
+      products.remove(product);
   }
 
   public void clear() {
+     products = null;
   }
 
 }

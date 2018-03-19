3cb689e8d6675ba1ed7f00d20f913cf915f7fd77
Andrei <casuneanu.andrei97@gmail.com>
2018-03-19 15:10:43 +0200

    added forward export

---
+++ forward/MarketPlace.java
@@ -0,0 +1,21 @@
+import java.util.Vector;
+
+public class MarketPlace {
+
+  private MarketPlace instance;
+
+  public java.util.Vector users;
+
+  public java.util.Vector productCollection;
+
+    public Vector  myProduct;
+    public Vector  myUser;
+
+  public MarketPlace getInstance() {
+  return null;
+  }
+
+  public void getUser(Integer id) {
+  }
+
+}
---
+++ forward/User.java
@@ -0,0 +1,20 @@
+import java.util.Vector;
+
+public class User {
+
+  public String name;
+
+  public Cart cart;
+
+  public Integer id;
+
+    public Vector  myMarketPlace;
+    public Vector  myCart;
+
+  public void addProduct(Integer id) {
+  }
+
+  public void removeProduct(Integer id) {
+  }
+
+}

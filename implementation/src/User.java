9ea2c5bd5d5db755e4984bf9e43a713406903757
MihaiStan72 <stan.mihai72@gmail.com>
2018-03-19 15:29:22 +0200

    implement user

--- implementation/src/User.java
+++ implementation/src/User.java
@@ -3,15 +3,11 @@ import java.util.Vector;
 public class User {
 
   public String name;
-
   public Cart cart;
-
   public Integer id;
 
-    public Vector  myMarketPlace;
-    public Vector  myCart;
-
   public void addProduct(Integer id) {
+    cart.addProduct()
   }
 
   public void removeProduct(Integer id) {
